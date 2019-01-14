dd44ecebed4d2fb16766157873087306289b74c3
Marcin Kopa <marcin.info@gmail.com>
2019-01-14 09:45:30 +0100

    Implementing checker

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function isUndefined(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Undefined]';
+}
+
+function isNull(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Null]';
+}
+
+function isBoolean(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Boolean]';
+}
+
+function isNumber(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Number]';
+}
+
+function isString(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object String]';
+}
+
+function isSymbol(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Symbol]';
+}
+
+function isObject(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Object]';
+}
+
+function isFunction(input) {
+  const type = Object.prototype.toString.call(input);
+  return type === '[object Function]' ||
+		type === '[object GeneratorFunction]' ||
+		type === '[object AsyncFunction]';
+}
+
+module.exports = {
+  isUndefined,
+  isNull,
+  isBoolean,
+  isNumber,
+  isString,
+  isSymbol,
+  isObject,
+  isFunction
+};
